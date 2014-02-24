4c8566709dbe6bcc3dbcaff6b6aa275f249d6cd4
Ryo Sakikawa <ryo.sakikawa@gmail.com>
2014-02-24 15:06:30 +0900

    initial

---
+++ app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ActiveRecord::Base
+  include FriendlyId
+
+  friendly_id :name, use: :slugged
+end
---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :price, :slug, :created_at, :updated_at
